21d808f0b42330358ff1bcee782e70a5ba067e2d
earl-stephens <stephensearl@hotmail.com>
2022-07-24 17:03:11 -0600

    Add comments explaining null pointer

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,21 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
+		
+		//int value = 5;
+		
+		//System.out.println(value/0);
+		//creates a run time exception
+		
+		Scanner scanner = null;
+		
+		scanner.nextLine();
+		//creates null pointer exception
+		//tried to call a method of a variable that is null
+		//variable hasn't been initialized
 		
 	}
 }
